2614cd477cad8c35f0e03f3d3e65486d13c01bcc
jigsawye <jigsaw.ye@gmail.com>
2015-09-21 16:06:46 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+var net = require('net');
+var process = require('process');
+var dgram = require('dgram');
+
+process.stdin.setEncoding('utf8');
+
+var client = [];
+
+var udp = dgram.createSocket('udp4');
+
+udp.on('message', function(msg, rinfo) {
+  console.log('UDP data: ', msg.toString());
+});
+
+var tcp = net.createServer(function(socket) {
+  console.log('CONNECTED:' + socket.remotePort + ' Port.');
+
+  client.push(sock);
+
+  socket.on('data', function(data) {
+    console.log('TCP data: ' + data);
+    socket.write('You said: ' + data);
+  });
+
+  socket.on('close', function(data) {
+    console.log('CLOSED: ' + socket.remotePort);
+  });
+
+});
+
+process.stdin.on('readable', function() {
+  var chunk = process.stdin.read();
+  if (chunk !== null) {
+    client[0].write(chunk);
+  }
+});
+
+udp.bind(9696, function() {
+  console.log('UDP Server listening on 9696 Port.');
+});
+
+tcp.listen(6969, function() {
+  console.log('TCP Server listening on 6969 Port.');
+});
